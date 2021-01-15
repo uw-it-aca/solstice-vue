7b2e1cf82f0b17270ee550fb8969d2f37c569f4e
Charlon Palacay <charlon@gmail.com>
2021-01-14 18:23:59 -0800

    Remove fullscreen layout parameter.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,6 @@ import '../src/assets/css/global.scss';
 /* use this parameter for now... need to figure out how
 to remove padding for layout stories ONLY */
 export const parameters = {
-  layout: 'fullscreen',
+  //layout: 'fullscreen',
   //docs: { inlineStories: false },
 };

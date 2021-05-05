75cebf88238b95acec6c735848e465664b7c71a3
Sarthak Singh <ss269@uw.edu>
2021-05-04 17:20:14 -0700

    Added a root index file to export the components

---
+++ index.js
@@ -0,0 +1,18 @@
+// Base Components
+import CardProperty from './src/_base/card-property.vue';
+import CardStatus from './src/_base/card-status.vue';
+import Card from './src/_base/card.vue';
+import LinkButton from './src/_base/link-button.vue';
+
+// Layout Components
+import Boilerplate from './src/_layouts/boilerplate/boilerplate.vue';
+import Sidebar from './src/_layouts/sidebar/index.vue';
+
+export default {
+    CardProperty,
+    CardStatus,
+    Card,
+    LinkButton,
+    Boilerplate,
+    Sidebar,
+}

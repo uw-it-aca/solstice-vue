1c62e6acc9c3115646e3e960d29bdf504769d97c
Charlon Palacay <charlon@gmail.com>
2021-05-20 11:18:04 -0700

    Updates Readme and home page.

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
 # axdd-components
 AXDD VueJS Component Library
 
-## Project setup
-```
-npm install
-```
+* Vue 2
+* Vite 2
+* Bootstrap 4
 
+
+## Getting Started
+
+> Note to Vue users: Vite currently only works with Vue 3.x. This project uses vite-plugin-vue2 as a work around.
+
+```
+$ git clone git@github.com:uw-it-aca/axdd-components.git
+$ cd axdd-components
+$ npm install
+
+```
 ### Compiles and hot-reloads for development
 ```
-npm run dev
+$ npm run dev
 ```
 
 ### Compiles and minifies for production
 ```
-npm run build
+$ npm run build
 ```
 
 ## Using Storybook
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -41,27 +41,6 @@
           </ul>
         </div>
         <div class="col-md-6">
-          <h2>Installed CLI Plugins</h2>
-          <ul>
-            <li>
-              <a
-                href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-                target="_blank"
-                rel="noopener"
-                >babel</a
-              >
-            </li>
-            <li>
-              <a
-                href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-                target="_blank"
-                rel="noopener"
-                >eslint</a
-              >
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-6">
           <h2>Examples</h2>
           <ul>
             <li>

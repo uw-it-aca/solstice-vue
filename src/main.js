5920c4dddebc97caea092dd82925a8d760878464
Charlon Palacay <charlon@gmail.com>
2021-07-19 21:39:25 -0700

    Fixes vuex store export.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from "./app.vue";
 import router from "./router";
-//import store from "./store";
+import store from "./store";
 
 import VueMq from 'vue3-mq';
 
@@ -26,6 +26,6 @@ app.use(VueMq, {
   },
 });
 app.use(router);
-//app.use(store);
+app.use(store);
 
 app.mount("#app");

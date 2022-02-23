aa43f3c28313eef9867e952cb891ef7f1e26de43
Charlon Palacay <charlon@gmail.com>
2022-02-22 21:07:22 -0800

    Updates sample home page content.

--- src/examples/my-app/home.vue
+++ src/examples/my-app/home.vue
@@ -4,22 +4,42 @@
     <template #title>{{ pageTitle }}</template>
     <template #content>
       <div class="card mb-3 bg-dark hover:bg-danger">
-        <div class="card-body text-white">asldkjfas</div>
+        <div class="card-body text-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta eveniet
+          dolorum harum eaque veniam nesciunt praesentium tempora ab illum in,
+          rem facere sit esse et? Similique deleniti blanditiis provident
+          perspiciatis.
+        </div>
       </div>
       <div class="row">
         <div class="col-md-4">
           <div class="card mb-3 bg-dark">
-            <div class="card-body text-white">asldkjfas</div>
+            <div class="card-body text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+              ex quod reprehenderit fugit culpa quas repudiandae numquam aut
+              dolor, quam impedit expedita labore? Nam blanditiis ea reiciendis
+              deleniti porro tempora.
+            </div>
           </div>
         </div>
         <div class="col-md-4">
           <div class="card mb-3">
-            <div class="card-body">asldkjfas</div>
+            <div class="card-body">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus
+              nobis quaerat minima illum nesciunt cupiditate sit, qui, non, sed
+              veritatis aut deleniti quia rem facere? Mollitia dolores obcaecati
+              maxime eius!
+            </div>
           </div>
         </div>
         <div class="col-md-4">
           <div class="card mb-3 border-white hover:border">
-            <div class="card-body">asldkjfas</div>
+            <div class="card-body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vero
+              animi illum omnis, deleniti quidem nulla qui ipsam molestias quo
+              saepe ipsum quibusdam nesciunt reiciendis dolorem cum tempore id
+              harum.
+            </div>
           </div>
         </div>
       </div>
--- src/examples/support-tools/home.vue
+++ src/examples/support-tools/home.vue
@@ -1,70 +1,45 @@
 <template>
   <axdd-layout page-title="Home">
-    <p class="card p-3">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde dolorum
-      voluptatem ipsa? Error earum hic nihil itaque ullam corporis eum, modi,
-      quaerat placeat velit numquam distinctio atque explicabo voluptatum nemo.
-    </p>
+    <div class="card mb-3 bg-dark hover:bg-danger">
+      <div class="card-body text-white">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+        nemo error quia vitae sequi tempore quidem commodi amet eveniet est
+        corporis, dignissimos repudiandae consequatur architecto in? A deleniti
+        pariatur voluptate.
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card mb-3 bg-dark">
+          <div class="card-body text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            distinctio id vitae labore est consectetur suscipit sed. Voluptate
+            quasi qui recusandae pariatur incidunt rerum accusamus dignissimos
+            sunt? Ex, perspiciatis excepturi!
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card mb-3">
+          <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            aperiam, explicabo deleniti officia nisi assumenda? Quae ducimus
+            deleniti deserunt! Unde reiciendis sint voluptates eos ex soluta sed
+            illo adipisci amet!
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card mb-3 border-white hover:border">
+          <div class="card-body">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam
+            deleniti fugit culpa tenetur illo. Ad quidem atque minima eos, error
+            modi et, asperiores mollitia nemo corrupti accusamus saepe earum
+            magni.
+          </div>
+        </div>
+      </div>
+    </div>
   </axdd-layout>
 </template>
 

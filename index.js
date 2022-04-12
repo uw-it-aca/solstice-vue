97e9d2995e2bb14ff822044a00bd0fcb3ebb8f23
Charlon Palacay <charlon@gmail.com>
2022-04-12 15:42:40 -0700

    Export tabs and card comps.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 // generic components
 import CardProperty from "./src/_components/card-property.vue";
 import CardStatus from "./src/_components/card-status.vue";
-import Card from "./src/_components/card.vue";
 import LinkButton from "./src/_components/link-button.vue";
 
-// layout components
+// 1.0.4
+import Card from "./src/_components/card/card.vue";
+import CardHeading from "./src/_components/card/heading.vue";
+import CardAction from "./src/_components/card/action.vue";
+import Tabs from "./src/_components/tabs/tabs.vue";
+import TabItem from "./src/_components/tabs/item.vue";
+import TabPanel from "./src/_components/tabs/panel.vue";
+
+// 1.0.0
 import Sidebar from "./src/_layouts/sidebar.vue";
 import Topbar from "./src/_layouts/topbar.vue";
 
-export { CardProperty, CardStatus, Card, LinkButton, Sidebar, Topbar };
+export {
+  CardProperty,
+  CardStatus,
+  LinkButton,
+  Card,
+  CardHeading,
+  CardAction,
+  Tabs,
+  TabItem,
+  TabPanel,
+  Sidebar,
+  Topbar,
+};

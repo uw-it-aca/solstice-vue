31833b2a757fb4b2b661d7251f20a298e5982d36
Charlon Palacay <charlon@gmail.com>
2022-04-14 11:59:46 -0700

    Adds additional card heading test.

--- src/_components/__tests__/card-heading.spec.js
+++ src/_components/__tests__/card-heading.spec.js
@@ -5,7 +5,11 @@ import CardHeading from "../card/heading.vue";
 
 describe("Card Heading", () => {
   it("renders correct element", () => {
-    const wrapper = mount(CardHeading, { props: { level: "3" } });
+    const wrapper = mount(CardHeading, { props: { level: 3 } });
     expect(wrapper.find("h3").exists()).toBe(true);
+  });
+  it("defaults to h2", () => {
+    const wrapper = mount(CardHeading, { props: { level: 10 } });
+    expect(wrapper.find("h2").exists()).toBe(true);
   });
 });

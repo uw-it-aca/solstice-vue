be9507217026008ebf65a4f10689fb0be87bb2b3
Charlon Palacay <charlon@gmail.com>
2022-04-15 14:38:16 -0700

    Fixes casing.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import Vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    Vue(),
     Pages({
       dirs: [
         { dir: "src/docs", baseRoute: "" },

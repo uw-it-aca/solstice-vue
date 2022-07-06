651c0c0c878a43ef5bcb810826ac3aceb0d2056f
Charlon Palacay <charlon@gmail.com>
2022-07-05 17:05:20 -0700

    Updates card comp styling.

--- src/_components/card/card.vue
+++ src/_components/card/card.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="card border-light-gray rounded-3 mb-4">
+  <div class="card border border-gray rounded-3 mb-4">
     <div
       v-if="$slots['heading']"
-      class="card-header border-0 bg-white p-4 pb-0"
+      class="card-header border-0 bg-white rounded-top p-4 pb-0"
     >
       <slot name="heading"></slot>
     </div>
     <div
       v-if="$slots['heading-action']"
-      class="card-header border-0 bg-white p-4 pb-0 d-flex justify-content-between"
+      class="card-header border-0 bg-white rounded-top p-4 pb-0 d-flex justify-content-between"
     >
       <slot name="heading-action"></slot>
     </div>
@@ -16,7 +16,10 @@
       <slot name="body"></slot>
     </div>
 
-    <div v-if="$slots['footer']" class="card-footer bg-white px-4 py-3 small">
+    <div
+      v-if="$slots['footer']"
+      class="card-footer bg-white border-top border-gray rounded-bottom px-4 py-3 small"
+    >
       <slot name="footer"></slot>
     </div>
   </div>

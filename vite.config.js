5cbf7984fe2f372eb5586ab6a1521894d3792cbb
Charlon Palacay <charlon@gmail.com>
2022-11-12 19:31:01 -0800

    Update package.json

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["vue"],
+      external: ["vue", "bootstrap"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps

0fe5f025f4b69c93512b3a9138089c422b75db64
Charlon Palacay <charlon@gmail.com>
2023-11-07 11:59:04 -0800

    Updates app title.

--- src/layout.vue
+++ src/layout.vue
@@ -42,7 +42,7 @@ export default {
   data() {
     return {
       // minimum application setup overrides
-      appName: "AXDD Components",
+      appName: "Solstice Vue",
     };
   },
   created: function () {

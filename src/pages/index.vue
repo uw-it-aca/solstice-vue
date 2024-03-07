7931ef3108a2f864bf9cf308e42c85aa73bd0e1a
Charlon Palacay <charlon@gmail.com>
2024-03-06 23:21:18 -0800

    Adds jsconfig. Updating link comp.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -142,30 +142,50 @@
       <div class="row mb-5">
         <div class="col">
           <h2 class="fw-bold">Elements</h2>
+
           <h3>Link</h3>
-          <ul>
-            <li>a: <a href="/">hello anchor</a></li>
-            <li>
-              default:
-              <sol-link :href="'/topbar'">hello world</sol-link>
-            </li>
-            <li>
-              quiet:
-              <sol-link :variant="'quiet'" :href="'/topbar'"
-                >hello world</sol-link
-              >
-            </li>
-            <li>
-              icon:
-              <sol-link
-                :variant="'icon'"
-                :href="'/topbar'"
-                :color="'link-danger'"
-                :icon="'bi-globe-americas'"
-                >hello world</sol-link
-              >
-            </li>
-          </ul>
+          <div class="d-flex">
+            <div class="w-100">
+              <p>Default</p>
+              <ul>
+                <li>a: <a href="/">hello anchor</a></li>
+                <li>
+                  default:
+                  <sol-link :href="'/topbar'">hello world</sol-link>
+                </li>
+                <li>
+                  quiet:
+                  <sol-link :variant="'quiet'" :href="'/topbar'"
+                    >hello world</sol-link
+                  >
+                </li>
+                <li>
+                  icon:
+                  <sol-link
+                    :variant="'icon'"
+                    :href="'/topbar'"
+                    :color="'link-danger'"
+                    :icon="'bi-globe-americas'"
+                    >hello world</sol-link
+                  >
+                </li>
+              </ul>
+            </div>
+            <div class="w-100">
+              <p>Dark</p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+              molestiae distinctio vel! Consectetur voluptatibus accusantium
+              quas! Distinctio aperiam quidem saepe asperiores consectetur
+              placeat quaerat repellat! Placeat nihil enim sequi quaerat.
+            </div>
+            <div class="w-100">
+              <p>Light</p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+              molestiae distinctio vel! Consectetur voluptatibus accusantium
+              quas! Distinctio aperiam quidem saepe asperiores consectetur
+              placeat quaerat repellat! Placeat nihil enim sequi quaerat.
+            </div>
+          </div>
         </div>
 
         <h3>Alerts</h3>

9173c6e4af577d58d4108ad094d836967540fa26
Charlon Palacay <charlon@gmail.com>
2024-04-09 09:33:52 -0700

    Updates layout with sidebar component list.

--- src/layout.vue
+++ src/layout.vue
@@ -1,31 +1,45 @@
 <template>
-  <div class="container-xl">
-    <div class="px-3">
-      <header
-        class="d-flex justify-content-between align-items-center pb-3 my-4 border-bottom"
-      >
-        <div class="h4 flex-fill">
-          <i class="bi bi-box-fill me-2 text-brand"></i>
-          <a href="/" class="text-reset text-decoration-none">{{ appName }}</a>
+  <div class="d-flex">
+    <div id="sidebar" class="fixed-top fixed-bottom" style="width: 250px">
+      <div class="vh-100 bg-body-tertiary p-3">
+        <header class="d-flex justify-content-between align-items-center">
+          <div class="h4 flex-fill">
+            <i class="bi bi-box-fill me-2 text-brand"></i>
+            <a href="/" class="text-reset text-decoration-none">{{
+              appName
+            }}</a>
+          </div>
+          <div><sol-color-mode></sol-color-mode></div>
+        </header>
+        <nav>
+          <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/link">Link</router-link></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <div style="margin-left: 250px">
+      <div class="container-xl">
+        <div class="px-3">
+          <main>
+            <h1 class="">
+              <slot name="title">
+                {{ pageTitle }}
+              </slot>
+            </h1>
+
+            <slot name="description" />
+            <hr class="mb-5 w-25 d-inline-block" />
+            <slot name="content" />
+          </main>
+
+          <footer class="pt-2 mt-5 mb-3 text-muted border-top">
+            Copyright &copy; {{ new Date().getFullYear() }} University of
+            Washington
+          </footer>
         </div>
-        <div><sol-color-mode></sol-color-mode></div>
-      </header>
-
-      <main>
-        <h1 class="">
-          <slot name="title">
-            {{ pageTitle }}
-          </slot>
-        </h1>
-
-        <slot name="description" />
-        <hr class="mb-5 w-25 d-inline-block" />
-        <slot name="content" />
-      </main>
-
-      <footer class="pt-2 mt-5 mb-3 text-muted border-top">
-        Copyright &copy; {{ new Date().getFullYear() }} University of Washington
-      </footer>
+      </div>
     </div>
   </div>
 </template>
---
+++ src/pages/link.vue
@@ -0,0 +1,78 @@
+// home.vue
+
+<template>
+  <layout :page-title="pageTitle">
+    <!-- page content -->
+    <template #title>
+      {{ pageTitle }}
+    </template>
+    <template #content>
+      <div class="row mb-5">
+        <div class="col">
+          <h2>Variants</h2>
+          <div class="d-flex">
+            <div class="w-100">
+              <p>Default</p>
+              <ul>
+                <li>a: <a href="/">hello anchor</a></li>
+                <li>
+                  default:
+                  <sol-link :href="'/topbar'">hello world</sol-link>
+                </li>
+                <li>
+                  quiet:
+                  <sol-link :variant="'quiet'" :href="'/topbar'"
+                    >hello world</sol-link
+                  >
+                </li>
+                <li>
+                  icon:
+                  <sol-link
+                    :variant="'icon'"
+                    :href="'/topbar'"
+                    :color="'link-danger'"
+                    :icon="'bi-globe-americas'"
+                    >hello world</sol-link
+                  >
+                </li>
+              </ul>
+            </div>
+            <div class="w-100">
+              <p>Dark</p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+              molestiae distinctio vel! Consectetur voluptatibus accusantium
+              quas! Distinctio aperiam quidem saepe asperiores consectetur
+              placeat quaerat repellat! Placeat nihil enim sequi quaerat.
+            </div>
+            <div class="w-100">
+              <p>Light</p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+              molestiae distinctio vel! Consectetur voluptatibus accusantium
+              quas! Distinctio aperiam quidem saepe asperiores consectetur
+              placeat quaerat repellat! Placeat nihil enim sequi quaerat.
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </layout>
+</template>
+
+<script>
+import Layout from "@/layout.vue";
+//import HelloWorld from "../components/hello-world.vue";
+
+export default {
+  name: "PagesLink",
+  components: {
+    layout: Layout,
+    //HelloWorld,
+  },
+  data() {
+    return {
+      pageTitle: "Link",
+    };
+  },
+  methods: {},
+};
+</script>

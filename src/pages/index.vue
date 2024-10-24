58645ee7f8886d98722eb3cf1c120e640bbfbe4f
Charlon Palacay <charlon@gmail.com>
2024-10-24 12:04:18 -0700

    Fix eslint errors.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -26,14 +26,13 @@
     <template #content>
       <div class="row mb-5">
         <div class="col">
-
           <nav aria-label="breadcrumb">
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="#">Home</a></li>
-    <li class="breadcrumb-item"><a href="#">Library</a></li>
-    <li class="breadcrumb-item active" aria-current="page">Data</li>
-  </ol>
-</nav>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"><a href="#">Library</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Data</li>
+            </ol>
+          </nav>
 
           <h2 class="fw-bold text-danger text-primary-hover">
             Building components

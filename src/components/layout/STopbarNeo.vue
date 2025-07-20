c2632005f481371f2abb632829be220457186b7d
Jim Laney <jlaney@uw.edu>
2025-07-20 09:21:39 -0700

    more flexible links for STopbarNeo footer

--- src/components/layout/STopbarNeo.vue
+++ src/components/layout/STopbarNeo.vue
@@ -112,15 +112,19 @@
               <div class="col small font-weight-light my-4">
                 <ul class="list-inline m-0">
                   <li class="list-inline-item">
-                    <a
-                      href="http://www.washington.edu/online/privacy/"
+                    <a v-if="privacyURL"
+                      :href="privacyURL"
+                      target="_blank"
+                      rel="noopener"
                       class="link-light"
                       >Privacy</a
                     >
                   </li>
                   <li class="list-inline-item">
-                    <a
-                      href="http://www.washington.edu/online/terms/"
+                    <a v-if="termsURL"
+                      :href="termsURL"
+                      target="_blank"
+                      rel="noopener"
                       class="link-light"
                       >Terms</a
                     >
@@ -157,6 +161,16 @@ export default {
     isPreview: {
       type: Boolean,
       default: false,
+    },
+    privacyURL: {
+      type: String,
+      required: false,
+      default: "https://www.washington.edu/online/privacy/",
+    },
+    termsURL: {
+      type: String,
+      required: false,
+      default: "https://www.washington.edu/online/terms/",
     },
   },
 };

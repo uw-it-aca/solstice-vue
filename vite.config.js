111cf05b91f917506cab851c264e1274add647b2
Charlon Palacay <charlon@gmail.com>
2025-10-29 22:45:54 -0700

    Adds types to package.json

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import VueRouter from "unplugin-vue-router/vite";
 import Components from "unplugin-vue-components/vite";
 import { BootstrapVueNextResolver } from "bootstrap-vue-next";
 import Vue from "@vitejs/plugin-vue";
-import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +14,6 @@ export default defineConfig({
     Components({
       resolvers: [BootstrapVueNextResolver()],
     }),
-    dts({}),
   ],
   resolve: {
     alias: {
